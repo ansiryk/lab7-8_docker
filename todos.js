bc5baaf431a3240170f18a9d8991818f78ae69fa
ansiryk <kbm241_sav@student.ztu.edu.ua>
2024-10-21 23:11:56 +0300

    Update todos.js

--- todos.js
+++ todos.js
@@ -1,23 +1,23 @@
 module.exports = [
   {
     id: 1,
-    task: "Fix Sink",
+    task: "Fix Sink",      // Завдання 1: Відремонтувати раковину
   },
   {
     id: 2,
-    task: "Buy Groceries",
+    task: "Buy Groceries", // Завдання 2: Купити продукти
   },
   {
     id: 3,
-    task: "Wash the dishes",
+    task: "Wash the dishes", // Завдання 3: Помити посуд
   },
-{
+  {
     id: 4,
-    task: "Sleep",
+    task: "Sleep",          // Завдання 4: Спати
   },
-{
+  {
     id: 5,
-    task: "Work",
-  },
-
+    task: "Work",           // Завдання 5: Працювати
+  }
 ];
+
